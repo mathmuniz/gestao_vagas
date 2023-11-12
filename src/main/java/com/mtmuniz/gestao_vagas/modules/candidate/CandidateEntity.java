02a54e7af6cdf6acecb5a0f25feced237eaed704
Matheus Muniz <mmuniz605@gmail.com>
2023-11-12 17:09:24 -0300

    feat: create CandidateEntity

---
+++ src/main/java/com/mtmuniz/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,23 @@
+package com.mtmuniz.gestao_vagas.modules.candidate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+}
