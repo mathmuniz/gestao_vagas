425ed63d785ad802e1a53ac50699ad74636a9e7b
Matheus Muniz <mmuniz605@gmail.com>
2023-12-08 16:04:23 -0300

    feat: add Validation

--- src/main/java/com/mtmuniz/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/mtmuniz/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,9 +1,13 @@
 package com.mtmuniz.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -15,8 +19,15 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espacos")
     private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
     private String email;
+
+    @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
     private String password;
     private String description;
     private String curriculum;
