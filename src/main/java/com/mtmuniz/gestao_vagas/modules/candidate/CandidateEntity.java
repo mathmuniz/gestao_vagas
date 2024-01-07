ff9846bcc6d2824908f838f637c56d97540a67b8
mathmuniz <mmuniz605@gmail.com>
2024-01-07 15:46:00 -0300

    create CandidateRepository

--- src/main/java/com/mtmuniz/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/mtmuniz/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,22 +1,25 @@
 package com.mtmuniz.gestao_vagas.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
@@ -31,4 +34,7 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/mtmuniz/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/mtmuniz/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,9 @@
 package com.mtmuniz.gestao_vagas.modules.candidate.controllers;
 
 import com.mtmuniz.gestao_vagas.modules.candidate.CandidateEntity;
+import com.mtmuniz.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository canditateRepository;
     @PostMapping("/")
-    public void create( @Valid @RequestBody CandidateEntity candidateEntity) {
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getEmail());
+    public CandidateEntity create( @Valid @RequestBody CandidateEntity candidateEntity) {
+        return this.canditateRepository.save(candidateEntity);
     }
 }
