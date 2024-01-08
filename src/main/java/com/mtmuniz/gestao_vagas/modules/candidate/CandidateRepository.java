7b7d564697f3e3b004cdc978fca8289053a5342f
mathmuniz <mmuniz605@gmail.com>
2024-01-08 08:47:09 -0300

    feat: implements findByUsernameOrEmail

--- src/main/java/com/mtmuniz/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/com/mtmuniz/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -1,8 +1,11 @@
 package com.mtmuniz.gestao_vagas.modules.candidate;
 
+import com.fasterxml.jackson.annotation.OptBoolean;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
 }
