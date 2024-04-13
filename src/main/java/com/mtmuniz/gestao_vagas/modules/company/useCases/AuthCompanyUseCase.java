38affd85e9aaea62531f290ea610fcef8b965e52
mathmuniz <mmuniz605@gmail.com>
2024-04-13 12:11:15 -0300

    feat: create Auths

---
+++ src/main/java/com/mtmuniz/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -0,0 +1,49 @@
+package com.mtmuniz.gestao_vagas.modules.company.useCases;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.mtmuniz.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import com.mtmuniz.gestao_vagas.modules.company.repositories.CompanyRepository;
+
+@Service
+public class AuthCompanyUseCase {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
+                () -> {
+                    throw new UsernameNotFoundException("Company not found!");
+                });
+
+        // Verify password are equals
+        var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+
+        // If password are not equals, throw exception
+        if (!passwordMatches) {
+            throw new AuthenticationException();
+        }
+        // If password are equals, generate token
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("javagas")
+        .withSubject(company.getId().toString())
+        .sign(algorithm);
+        return token;
+    }
+
+}
