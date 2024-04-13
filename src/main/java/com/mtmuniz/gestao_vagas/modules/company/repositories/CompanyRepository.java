b65859ee8eb7fd2bba0c5df4b261d8ecccecaa0b
mathmuniz <mmuniz605@gmail.com>
2024-04-13 12:09:45 -0300

    feat: add findByUsername

--- src/main/java/com/mtmuniz/gestao_vagas/modules/company/repositories/CompanyRepository.java
+++ src/main/java/com/mtmuniz/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
+
 public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
     Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+
+    Optional<CompanyEntity> findByUsername(String username);
 }
