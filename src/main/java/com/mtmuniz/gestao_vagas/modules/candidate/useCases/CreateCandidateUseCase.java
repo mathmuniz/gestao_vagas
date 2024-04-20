cef3dbc62125ac95191f522ba0e3facec67eb3f6
mathmuniz <mmuniz605@gmail.com>
2024-04-20 13:38:31 -0300

    feat: encrypted password

--- src/main/java/com/mtmuniz/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/com/mtmuniz/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import com.mtmuniz.gestao_vagas.exceptions.UserFoundException;
 import com.mtmuniz.gestao_vagas.modules.candidate.entities.CandidateEntity;
 import com.mtmuniz.gestao_vagas.modules.candidate.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -13,10 +14,16 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent((user) -> {
             throw new UserFoundException();
         });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }
